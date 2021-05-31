42e7cf1235809b925deb0847db94d718e6cd1eab
Anthony Ringoet <anthonyringoet@gmail.com>
2021-05-31 09:42:53 +0200

    brew: add edge

--- brew.sh
+++ brew.sh
@@ -110,6 +110,7 @@ brew install --cask rectangle
 brew install --cask lens
 brew install --cask docker
 brew install --cask firefox
+brew install --cask microsoft-edge
 brew install --cask iterm2
 brew install --cask keepingyouawake
 brew install --cask karabiner-elements
